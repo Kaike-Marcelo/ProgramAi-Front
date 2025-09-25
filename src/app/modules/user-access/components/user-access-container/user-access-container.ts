d5aaea19783e17238e6034840a12a935f34ebe84
Kaike-Marcelo <kaikeemarceloo@gmail.com>
2025-09-25 12:44:07 -0300

    feat: Configuração do ambiente, adição do Tailwind e DaisyUI, e adição do módulo de acesso do usuário

---
+++ src/app/modules/user-access/components/user-access-container/user-access-container.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-access-container',
+  imports: [],
+  templateUrl: './user-access-container.html'
+})
+export class UserAccessContainer {
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+const environment = {
+    production: false,
+    apiUrl: 'https://localhost:8080/api',
+    appUrl: 'https://localhost:4200',
+};
