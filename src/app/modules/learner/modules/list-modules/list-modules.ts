48be2b7dad6c1148d4a6fa5992f5a8d3294b8ce0
Kaike-Marcelo <kaikeemarceloo@gmail.com>
2025-12-01 08:42:48 -0300

    style: ajuste na estrutura do HTML para melhor acessibilidade e responsividade dos módulos

---
+++ src/app/modules/learner/modules/list-modules/list-modules.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-modules',
+  imports: [],
+  templateUrl: './list-modules.html',
+})
+export class ListModules {
+
+}
---
+++ src/app/modules/learner/modules/modules.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modules',
+  imports: [],
+  templateUrl: './modules.html',
+})
+export class Modules {
+
+}
