3aafa703ad9ac673eba463225cabe80598289bc8
Kaike-Marcelo <kaikeemarceloo@gmail.com>
2026-01-17 19:13:00 -0300

    style: Definido tooltip para ficar no topo do botão de edição e removido limite máximo de expansão do game-frame

--- src/app/shared/components/simple-components/game-frame/game-frame.ts
+++ src/app/shared/components/simple-components/game-frame/game-frame.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   imports: [],
   template: `
     <div [class]="bgClass + ' ' + borderClass"
-         class="relative border-4 rounded-xl p-4 sm:p-6 shadow-[0_0_20px_rgba(0,0,0,0.5)] backdrop-blur-sm w-full max-w-3xl flex flex-col md:flex-row items-center gap-4 sm:gap-8">
+         class="relative border-4 rounded-xl p-4 sm:p-6 shadow-[0_0_20px_rgba(0,0,0,0.5)] backdrop-blur-sm w-full flex flex-col md:flex-row items-center gap-4 sm:gap-8">
       <div [class]="'absolute top-1.5 left-1.5 w-2 h-2 sm:w-3 sm:h-3 ' + nailHeadsColor + ' rounded-full shadow-inner'"></div>
       <div [class]="'absolute top-1.5 right-1.5 w-2 h-2 sm:w-3 sm:h-3 ' + nailHeadsColor + ' rounded-full shadow-inner'"></div>
       <div [class]="'absolute bottom-1.5 left-1.5 w-2 h-2 sm:w-3 sm:h-3 ' + nailHeadsColor + ' rounded-full shadow-inner'"></div>
