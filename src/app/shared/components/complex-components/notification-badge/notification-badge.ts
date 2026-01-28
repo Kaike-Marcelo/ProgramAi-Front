b7ffcad3f363bb3ff7cf8b6a1d71ce710d45ae22
Kaike-Marcelo <kaikeemarceloo@gmail.com>
2026-01-28 11:59:08 -0300

    feat: adicionar suporte a polling de notificações com intervalos configuráveis

--- src/app/shared/components/complex-components/notification-badge/notification-badge.ts
+++ src/app/shared/components/complex-components/notification-badge/notification-badge.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { NotificationStore } from '../../../stores/notification/notification.store';
 import { NotificationActions } from '../../../stores/notification/notification.actions';
 import { RouterLink } from "@angular/router";
@@ -9,11 +9,12 @@ import { DatePipe } from '@angular/common';
   imports: [RouterLink, DatePipe],
   templateUrl: './notification-badge.html',
 })
-export class NotificationBadge implements OnInit {
+export class NotificationBadge implements OnInit, OnDestroy {
   @Input() buttonClass: string = 'btn-ghost';
   @Input() dropdownPosition: 'dropdown-start' | 'dropdown-end' | 'dropdown-center' = 'dropdown-start';
   @Input() dropdownDirection: 'dropdown-bottom' | 'dropdown-top' | 'dropdown-left' | 'dropdown-right' = 'dropdown-bottom';
-
+  @Input() enablePolling: boolean = true;
+  @Input() pollingInterval: number = 30000;
 
   #notificationStore = inject(NotificationStore);
   #notificationActions = inject(NotificationActions);
@@ -25,7 +26,15 @@ export class NotificationBadge implements OnInit {
   maxNumberOfNotifications: number = 10;
 
   ngOnInit(): void {
-    this.#notificationActions.loadUnreadNotifications(this.maxNumberOfNotifications);
+    if (this.enablePolling) {
+      this.#notificationActions.startPolling(this.maxNumberOfNotifications, this.pollingInterval);
+    } else {
+      this.#notificationActions.loadUnreadNotifications(this.maxNumberOfNotifications);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.#notificationActions.stopPolling();
   }
 
   markAsRead(id: number): void {
